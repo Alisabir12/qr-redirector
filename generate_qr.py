dedcedc1f68b7851efa93e971fc5231a50970268
Alisabir12 <2ab22cs400@anjuman.edu.in>
2025-06-01 11:47:29 +0530

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -2,7 +2,7 @@
 import qrcode
 
 # URL of your Flask redirector (deployed version if using online)
-redirector_url = "http://localhost:5000"  # change if hosted online
+redirector_url = "https://qr-redirector-2wwg.onrender.com"  # change if hosted online
 
 img = qrcode.make(redirector_url)
 img.save("redirect_qr.png")
